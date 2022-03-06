c5b177274a81c2839eb4f8a746c9deb30c03089d
manInit <info.denisbronnikov@gmail.com>
2022-03-06 16:59:26 +0800

    404 like template

--- 404.php
+++ 404.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>404</title>
-</head>
-<body>
-  <h1>Запрашиваемая страница не найдена</h1>
-</body>
-</html>
+<?php
+
+require_once './routing/Router.php';
+require_once './constants.php';
+
+Router::showTemplate('404', []);
+die();
